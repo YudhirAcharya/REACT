02fac75919c5467c264321b5b1389301fa8055cd
Yudhir <121503369+YudhirAcharya@users.noreply.github.com>
2023-06-24 21:47:28 +0545

    d24.last

--- vite_app/src/App.jsx
+++ vite_app/src/App.jsx
@@ -1,34 +1,32 @@
 import React from "react";
 import { useState } from "react";
-import { people } from "./data";
+import { useEffect } from "react";
 import "./app.css";
 
 const App = () => {
-  const [person, setPerson] = useState({
-    name: "peter",
-    age: "20",
-    hobby: "art",
-  });
-
-  const newPerson = () => {
-    setPerson({
-      name: "harry",
-      age: "25",
-      hobby: "football",
-    });
-  };
   const [increment, setValue] = useState(0);
+  const [increment2, setValue2] = useState(0);
+
   const incrementValue = () => {
     setValue(increment + 1);
   };
+  const incrementValue2 = () => {
+    setValue2(increment2 + 1);
+  };
+
+  useEffect(() => {
+    console.log("use effect 1");
+  }, [increment]);
+  useEffect(() => {
+    console.log("use effect 2");
+  }, [increment2]);
+
   return (
     <>
-      <h2>{person.name}</h2>
-      <h2>{person.age}</h2>
-      <h2>{person.hobby}</h2>
-      <button onClick={newPerson}>change</button>
       <h2>increment: {increment}</h2>
       <button onClick={incrementValue}>increment</button>
+      <h2>increment2: {increment2}</h2>
+      <button onClick={incrementValue2}>increment2</button>
     </>
   );
 };
