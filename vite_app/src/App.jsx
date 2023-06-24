4f7608c49d57d8957c8c8be773126316d42f7e94
Yudhir <121503369+YudhirAcharya@users.noreply.github.com>
2023-06-24 20:48:34 +0545

    d24

--- vite_app/src/App.jsx
+++ vite_app/src/App.jsx
@@ -1,24 +1,35 @@
+import React from "react";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { people } from "./data";
+import "./app.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: "20",
+    hobby: "art",
+  });
 
-  const CountVar = () => {
-    setCount(count + 1);
+  const newPerson = () => {
+    setPerson({
+      name: "harry",
+      age: "25",
+      hobby: "football",
+    });
+  };
+  const [increment, setValue] = useState(0);
+  const incrementValue = () => {
+    setValue(increment + 1);
   };
   return (
     <>
-      <h1>Vite App</h1>
-      <button id="count-btn" onClick={CountVar}>
-        Increase Count
-      </button>{" "}
-      <h2 id="count">Count: {count}</h2>
-      <img className="logo" src={reactLogo}></img>
+      <h2>{person.name}</h2>
+      <h2>{person.age}</h2>
+      <h2>{person.hobby}</h2>
+      <button onClick={newPerson}>change</button>
+      <h2>increment: {increment}</h2>
+      <button onClick={incrementValue}>increment</button>
     </>
   );
-}
-
+};
 export default App;
